3e13b7eebfeaaf160ba98125bbc4b2d225b385bc
will-Rberry <1116094@apps.nsd.org>
2025-09-17 15:42:22 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,5 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package org.nhs.app;
-
-// Your code goes here
+class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
